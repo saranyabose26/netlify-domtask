5e6d208af82cee8ccb1e024ab62fc84d126dbbec
saranya <147645957+saranyabose26@users.noreply.github.com>
2023-12-17 12:23:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function foo(){
+    var fname=document.getElementById("first").value;
+    console.log("firstname",fname);
+    var Mname=document.getElementById("Middle").value;
+    console.log("middlename",Mname);
+    var lname=document.getElementById("last").value;
+    console.log("lastname",lname);
+    var email = document.getElementById("email").value;
+    console.log("email",email);
+    }
